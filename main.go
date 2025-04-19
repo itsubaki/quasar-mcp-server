c1e6f901ada63ef0a5bb00bc4a48e8577927b5f9
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-19 17:32:05 +0900

    Update some files

--- main.go
+++ main.go
@@ -30,12 +30,13 @@ func main() {
 			mcp.WithDescription("factorize a number using shor's algorithm"),
 			mcp.WithNumber("N",
 				mcp.Required(),
-				mcp.Description("the number to factorize"),
+				mcp.Description("the number to factorize (integer)"),
 			),
 		),
 		func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 			// parameters
-			N, t, a := int(req.Params.Arguments["N"].(float64)), 3, 2
+			N := int(req.Params.Arguments["N"].(float64))
+			t, a := 4, -1
 			var seed uint64
 
 			// factorization
@@ -47,7 +48,11 @@ func main() {
 			}
 
 			// response
-			msg := fmt.Sprintf("The prime factorization of %v is %v and %v.", resp.N, resp.P, resp.Q)
+			msg := strings.Join([]string{
+				fmt.Sprintf("The prime factorization of %v is %v and %v.", resp.N, resp.P, resp.Q),
+				fmt.Sprintf("num of precision qubits=%v, coprime number of N=%v, PRNG seed=%v, measured bitstring=%v, s/r=%v", resp.T, resp.A, resp.Seed, resp.M, resp.SR),
+			}, "\n")
+
 			if resp.P == 0 || resp.Q == 0 {
 				msg = strings.Join([]string{
 					"The operation failed.",
