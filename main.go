acb5e47f2b892d7416d4f152099cd023b03d2909
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-04-19 13:37:37 +0900

    Update tool

--- main.go
+++ main.go
@@ -25,39 +25,40 @@ func main() {
 		server.WithRecovery(),
 	)
 
-	tool := mcp.NewTool("factorization",
-		mcp.WithDescription("factorize a number using shor's algorithm"),
-		mcp.WithNumber("N",
-			mcp.Required(),
-			mcp.Description("the number to factorize"),
+	s.AddTool(
+		mcp.NewTool("factorization",
+			mcp.WithDescription("factorize a number using shor's algorithm"),
+			mcp.WithNumber("N",
+				mcp.Required(),
+				mcp.Description("the number to factorize"),
+			),
 		),
+		func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+			// parameters
+			N, t, a := int(req.Params.Arguments["N"].(float64)), 3, 2
+			var seed uint64
+
+			// factorization
+			resp, err := client.
+				New(BaseURL, IdentityToken).
+				Factorize(ctx, N, t, a, seed)
+			if err != nil {
+				return nil, fmt.Errorf("factorize: %w", err)
+			}
+
+			// response
+			msg := fmt.Sprintf("The prime factorization of %v is %v and %v.", resp.N, resp.P, resp.Q)
+			if resp.P == 0 || resp.Q == 0 {
+				msg = strings.Join([]string{
+					"The operation failed.",
+					"Please try again.",
+					"Since quantum computation rely on probabilistic algorithms, correct results are not always guaranteed.",
+				}, "\n")
+			}
+
+			return mcp.NewToolResultText(msg), nil
+		},
 	)
-
-	s.AddTool(tool, func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		// parameters
-		N, t, a := int(req.Params.Arguments["N"].(float64)), 3, 2
-		var seed uint64
-
-		// factorization
-		resp, err := client.
-			New(BaseURL, IdentityToken).
-			Factorize(ctx, N, t, a, seed)
-		if err != nil {
-			return nil, fmt.Errorf("factorize: %w", err)
-		}
-
-		// response
-		msg := fmt.Sprintf("The prime factorization of %v is %v and %v.", resp.N, resp.P, resp.Q)
-		if resp.P == 0 || resp.Q == 0 {
-			msg = strings.Join([]string{
-				"The operation failed.",
-				"Please try again.",
-				"Since quantum computation rely on probabilistic algorithms, correct results are not always guaranteed.",
-			}, "\n")
-		}
-
-		return mcp.NewToolResultText(msg), nil
-	})
 
 	// start
 	if err := server.ServeStdio(s); err != nil {
